2f092012c78dbcf43d7c29f2577c07c9a7aba876
arslansherazi <arslanhaider.dev@hotmail.com>
2022-03-24 13:22:47 +0500

    dynamic array and array slicing

--- 3_data_structures/2_arrays.go
+++ 3_data_structures/2_arrays.go
@@ -14,14 +14,15 @@ func printArray(array []int) {
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	min := 10
+	max := 30
+
 	// 1D arrays
 	var numbers = []int{20, 30, 40, 50, 60} // size of array = no of initialized array elements
 	printArray(numbers)
 
 	var nums [10]int
-	rand.Seed(time.Now().UnixNano())
-	min := 10
-	max := 30
 	for i := 0; i < len(nums); i++ {
 		random_number := rand.Intn(max-min+1) + min
 		nums[i] = random_number
@@ -45,7 +46,27 @@ func main() {
 
 	// iterate through array
 	var names = []string{"Arslan", "Waqar", "Fasi", "Haider"}
-	for index := range names {
-		fmt.Println(names[index])
+	for index, element := range names {
+		fmt.Printf("index=%d, element=%s \n", index, element)
 	}
+
+	// Dynamic size array
+	var random_numbers []int
+	var count int
+	fmt.Print("Enter count::")
+	fmt.Scanln(&count)
+	for i := 0; i <= count; i++ {
+		random_number := rand.Intn(max-min+1) + min
+		random_numbers = append(random_numbers, random_number)
+	}
+	fmt.Println(random_numbers)
+
+	// slicing array
+	var numbers2 = []int{11, 23, 45, 67, 24, 78, 94, 20, 12, 34, 90, 45}
+	array_slice_1 := numbers2[2:5]
+	array_slice_2 := numbers2[5:]
+	array_slice_3 := numbers2[:5]
+	fmt.Println(array_slice_1)
+	fmt.Println(array_slice_2)
+	fmt.Println(array_slice_3)
 }
